2e59a4c2885519cb6973d13e93cd57ba8cd72e9f
LewisB13 <lewisb13@protonmail.com>
2025-09-08 12:38:14 +0100

    added list

---
+++ Month 1/Week 1/Python code/TaskLogger.py
@@ -0,0 +1,14 @@
+#creae list
+dailyTasks = ["shave", "shower", "poop", "clean Teeth", 
+              "breakfast", "work", "games", "sleep"]
+#print index [0] is first 
+print (dailyTasks[0])
+print (dailyTasks[1])
+print (dailyTasks[2])
+print (dailyTasks[3])
+print (dailyTasks[4])
+print (dailyTasks[5])
+#print out from last index usign -1 -2 03 etc -1 being last index -2 second last etc etc
+print (dailyTasks[-3])
+print (dailyTasks[-2])
+print (dailyTasks[-1])
