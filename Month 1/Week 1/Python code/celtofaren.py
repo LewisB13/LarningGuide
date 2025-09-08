5a5b9655915f8161a0adba2d520d2e27e7de80c4
LewisB13 <lewisb13@protonmail.com>
2025-09-08 11:50:58 +0100

    temp converter
    files moved into code file not by day and thursday code for farenheit to celcius and celcius to farenheit complete

---
+++ Month 1/Week 1/Python code/celtofaren.py
@@ -0,0 +1,8 @@
+#ask for degrees
+celcius = input("please insert degrees in celcius: ")
+
+#formula for getting cel to farenheit
+farenheit = (int(celcius)*(9/5)+ 32)
+
+#print
+print("It is ", farenheit, "farenheit")
---
+++ Month 1/Week 1/Python code/farenheitToCelcius.py
@@ -0,0 +1,10 @@
+#ask for degrees in farenheit
+
+farenheit = input("How many degrees is it ")
+
+#formula for farenheit to celcius
+
+celcius = ((float(farenheit)-32) / (9/5))
+
+#print result 
+print ("it is ", celcius, "degrees ")
