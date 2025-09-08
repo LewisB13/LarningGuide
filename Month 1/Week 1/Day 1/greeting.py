a099e14924d06b03721ff974e1c755a6d57c617f
LewisB13 <lewisb13@protonmail.com>
2025-09-08 11:23:04 +0100

    added greeting

---
+++ Month 1/Week 1/Day 1/greeting.py
@@ -0,0 +1,9 @@
+#ask and input fname as variable  fname 
+fname= input("Hey what is your name: ")
+
+#print name and then ask for surname 
+print("hey", fname)
+sname = input("what is your surname ")
+
+#print both together in 1 statment 
+print ("nice to meet you " , fname, sname, "my name is python how are you ")
